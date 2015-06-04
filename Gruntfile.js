908deae6a553327676ca1e0eef637432f87773f8
alejandrodob <alejandrodob@gmail.com>
2015-06-04 18:19:14 +0200

    Add watch and jshint to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,10 +3,30 @@ module.exports = function(grunt) {
   grunt.initConfig({
     qunit: {
       all: ['tests/index.html']
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'pybossa.js', 'tests/**/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint', 'qunit']
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-qunit');
 
+  grunt.registerTask('default', ['jshint', 'qunit']);
   grunt.registerTask('test', ['qunit']);
+  grunt.registerTask('dev', ['watch', 'jshint', 'qunit']);
 }
